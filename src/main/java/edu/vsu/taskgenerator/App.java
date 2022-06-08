f284eb560c37e46c02e5dcc43fe00c0c404d594f
Anton Kolesov <kolesov.anton.s@gmail.com>
2022-06-08 19:25:19 +0300

    Init commit

---
+++ src/main/java/edu/vsu/taskgenerator/App.java
@@ -0,0 +1,8 @@
+package edu.vsu.taskgenerator;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/edu/vsu/taskgenerator/AppTest.java
@@ -0,0 +1,12 @@
+package edu.vsu.taskgenerator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
