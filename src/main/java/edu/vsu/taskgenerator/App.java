d095901650abbfb493c166917b85923575aa4b81
Denis <denislisaev@yandex.ru>
2022-06-30 10:26:37 +0300

    JavaFX was configured

--- src/main/java/edu/vsu/taskgenerator/App.java
+++ src/main/java/edu/vsu/taskgenerator/App.java
@@ -1,8 +1,17 @@
 package edu.vsu.taskgenerator;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+
+public class App extends Application{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
