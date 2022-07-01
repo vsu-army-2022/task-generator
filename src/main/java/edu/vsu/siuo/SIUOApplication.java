0d98d1339e149a53853a7d59f8b0d510ed81cdf1
Denis <denislisaev@yandex.ru>
2022-07-01 16:23:53 +0300

    UI v0.0.2 updated

--- src/main/java/edu/vsu/siuo/SIUOApplication.java
+++ src/main/java/edu/vsu/siuo/SIUOApplication.java
@@ -3,21 +3,30 @@ package edu.vsu.siuo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SIUOApplication extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        setPrimaryStage(stage);
         FXMLLoader fxmlLoader = new FXMLLoader(SIUOApplication.class.getResource("mainPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Генератор вариантов для СиУО");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void setPrimaryStage(Stage stage) {
+        SIUOApplication.primaryStage = stage;
+    }
+
+    static public Stage getPrimaryStage() {
+        return SIUOApplication.primaryStage;
     }
 
     public static void main(String[] args) {
