22664cca7f816eef6e7faf1c7b97ad1ba0c36288
pervitskiy <pervitsky@cs.vsu.ru>
2022-07-08 09:03:29 +0300

    add wordManager

---
+++ src/main/java/edu/vsu/siuo/word/WordManager.java
@@ -0,0 +1,116 @@
+package edu.vsu.siuo.word;
+
+
+import com.spire.doc.*;
+import com.spire.doc.documents.HorizontalAlignment;
+import com.spire.doc.documents.Paragraph;
+import com.spire.doc.documents.TableRowHeightType;
+import com.spire.doc.documents.VerticalAlignment;
+import com.spire.doc.fields.TextRange;
+import edu.vsu.siuo.domains.TaskDto;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class WordManager {
+    private String fileName;
+    private Document document;
+
+    public WordManager(String fileName) {
+        this.document = new Document();
+        this.fileName = fileName;
+        new File(fileName);
+    }
+
+    public void Write(TaskDto taskDto){
+        Section sec = this.document.addSection();
+        //add a paragraph
+        Paragraph paragraph = sec.addParagraph();
+        TextRange tr = paragraph.appendText("Тема 7. Вариант №1 от 04.07.2022 - 10:14");
+        tr.getCharacterFormat().setFontSize(12);
+        tr.getCharacterFormat().setBold(true);
+        paragraph.getFormat().setAfterSpacing(10f);
+
+        paragraph = sec.addParagraph();
+        tr = paragraph.appendText("Батарея 122мм гаубицы Д-30 занимает боевой порядок:\n" +
+                "ОП:  \tХ = 32290; \tУ = 91238; \th = 170 м («Дон»)\n" +
+                "КНП: \tХ = 30031; \tУ = 95266; \th = 77 м («Амур»)\n" +
+                "КНП адн: Х = 30031; У = 95266; \th = 77 м («Лена»)\n" +
+                "α он = 16-00\n" +
+                "В батарее рассчитаны поправки для заряда «2» на 4, 6, 8 км.\n" +
+                "В дальности: +133; +288; +406. В направлении: -0-09; -0-13; -0-16.\n" +
+                "Командир дивизиона («Лена») передал: «Амур», стой! Цель 21, «радиолокационная станция полевой артиллерии». Дивизионный: αц = 8-66, Дк = 2201, εц = +0-09. Подавить! Я «Лена».\n" +
+                "В должности командира батареи провести пристрелку и стрельбу на поражение цели 21, если в ходе стрельбы получены следующие наблюдения:\n" +
+                "1) Л77, «-»; 2) П42, «+»; 3) П18, «+»; 4) П20, Все «+», Фр. 0-06; 5) П11, Преобладание «+», Фр. 0-12; 6) Цель подавлена. \n");
+        tr.getCharacterFormat().setFontSize(12);
+        paragraph.getFormat().setLineSpacing(5);
+
+        paragraph = sec.addParagraph();
+        tr = paragraph.appendText("Тема 7. Вариант №1 от 04.07.2022 - 10:14");
+        tr.getCharacterFormat().setFontSize(12);
+        tr.getCharacterFormat().setBold(true);
+        paragraph.getFormat().setAfterSpacing(10f);
+
+        paragraph = sec.addParagraph();
+        tr = paragraph.appendText("Дцт = 5831\t∆Д = +295\tДци = 6126\t∂цт = +0-48\t∆∂ = -0-13\t∂ци = +0-35\n" +
+                "ПС = 7-82\tОП (кнп) - Слева\t∆Xтыс = 15\tВд = 14");
+        tr.getCharacterFormat().setFontSize(12);
+        paragraph.getFormat().setLineSpacing(5);
+
+
+        Section section = document.addSection();
+
+        //Define the data for table
+        String[] header = {"№", "Команда на ОП.", "Пр.", "Ур.", "Дов.", "Наблюдения."};
+        String[][] data =
+                {
+                        new String[]{"1", "Argentina", "Buenos Aires", "South America", "2777815", "32300003"},
+                        new String[]{"2","Bolivia", "La Paz", "South America", "1098575", "7300000"},
+                        new String[]{"3","Brazil", "Brasilia", "South America", "8511196", "150400000"},
+                        new String[]{"4","Canada", "Ottawa", "North America", "9976147", "26500000"},
+                };
+
+        //Add a table
+        Table table = section.addTable(true);
+        table.resetCells(data.length + 1, header.length);
+
+        //Set the first row as table header
+        TableRow row = table.getRows().get(0);
+        row.isHeader(true);
+        row.setHeight(20);
+        row.setHeightType(TableRowHeightType.Exactly);
+//        row.getRowFormat().setBackColor(Color.gray);
+        for (int i = 0; i < header.length; i++) {
+            row.getCells().get(i).getCellFormat().setVerticalAlignment(VerticalAlignment.Middle);
+            Paragraph p = row.getCells().get(i).addParagraph();
+            p.getFormat().setHorizontalAlignment(HorizontalAlignment.Center);
+            TextRange txtRange = p.appendText(header[i]);
+            txtRange.getCharacterFormat().setBold(true);
+        }
+
+        //Add data to the rest of rows
+        for (int r = 0; r < data.length; r++) {
+            TableRow dataRow = table.getRows().get(r + 1);
+            dataRow.setHeight(25);
+            dataRow.setHeightType(TableRowHeightType.Exactly);
+//            dataRow.getRowFormat().setBackColor(Color.GRAY);
+            for (int c = 0; c < data[r].length; c++) {
+                dataRow.getCells().get(c).getCellFormat().setVerticalAlignment(VerticalAlignment.Middle);
+                dataRow.getCells().get(c).addParagraph().appendText(data[r][c]);
+            }
+        }
+
+
+        //save
+        this.document.saveToFile(fileName, FileFormat.Docm_2010);
+    }
+
+    public static String GenerateNameFile(String taskTopic) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy hh-mm");
+        String text = dtf.format( LocalDateTime.now() );
+        String fileName = String.format("%s %s.docx", taskTopic, text);
+        return fileName;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module edu.vsu.siuo {
     requires javafx.controls;
     requires javafx.fxml;
     requires lombok;
+    requires spire.doc.free;
+    requires java.desktop;
 
     opens edu.vsu.siuo to javafx.fxml;
     exports edu.vsu.siuo;
