cae093457203444efad12c87e810d1c745ee7b78
weldnor <kolesov.anton.s@gmail.com>
2022-07-13 10:28:03 +0300

    Added generateShotDalnomer method

---
+++ src/test/java/edu/vsu/siuo/GenerateDalnomerMore5Test.java
@@ -0,0 +1,22 @@
+package edu.vsu.siuo;
+
+import org.junit.jupiter.api.Test;
+
+class GenerateDalnomerMore5Test {
+
+    @Test
+    void generateTasks() {
+    }
+
+    @Test
+    void generateTask() {
+    }
+
+    @Test
+    void generateConditionsForTask() {
+    }
+
+    @Test
+    void generateSolution() {
+    }
+}
